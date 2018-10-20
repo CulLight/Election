88da8f88bc7051d4347abb9a7aa6dc5f655f1044
Lucas Thiel <lucas.thiel@unibas.ch>
2018-10-20 23:48:17 +0200

    3. cast votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -9,6 +9,8 @@ contract Election{
 	}
 	// Store Candidates
 	
+	// Store accounts that have voted
+	mapping (address => bool) public voters;
 	// Fetch Candidate
 	// there is no way to get length of mapping and to iterate over it.
 	// because for every non valid key, we still get an empty value (candidate)
@@ -16,7 +18,7 @@ contract Election{
 	mapping (uint => Candidate) public candidates;
 	
 	// Store Candidates Count, set to 0 by default
-	uint public candidatesCount;
+	uint public candidatesCount; 
 	
 	function Election () public {
 		// code is run when smart contract is deployed to blockchain
@@ -28,5 +30,17 @@ contract Election{
 	function addCandidate (string _name) private {
 		candidatesCount ++;
 		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+	
+	function vote (uint _candidateId) public {
+		// require will send back unused gas
+		// check that voter has not voted already and candidateId is valid
+		require(!voters[msg.sender]);
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+		
+		// record that voter has voted
+		voters[msg.sender] = true;
+		// update candidate vote count
+		candidates[_candidateId].voteCount ++;
 	}
 }
