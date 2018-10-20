73083fa9abd5eb1c240cbb24fc046fff22d97ca9
Lucas Thiel <lucas.thiel@unibas.ch>
2018-10-20 22:13:08 +0200

    2. index.html working

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,32 @@
 pragma solidity ^0.4.2;
 
 contract Election{
-	// read/write candiate
-	// declaring candidate public will make a getter function with same name
-	string public candidate; // state variable for whole contract
+	// Model a Candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	// Store Candidates
 	
-	// Contstructor
-	function Election () public{
-		candidate = "Cadidate 1";
+	// Fetch Candidate
+	// there is no way to get length of mapping and to iterate over it.
+	// because for every non valid key, we still get an empty value (candidate)
+	// need to keep track with candidates Count
+	mapping (uint => Candidate) public candidates;
+	
+	// Store Candidates Count, set to 0 by default
+	uint public candidatesCount;
+	
+	function Election () public {
+		// code is run when smart contract is deployed to blockchain
+		addCandidate("Markus Hasler");
+		addCandidate("Ronny Rueffel");		
+	}
+	
+	// local variable gets underscore
+	function addCandidate (string _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 }
